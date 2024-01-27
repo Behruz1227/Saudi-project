bb5c4a7c0aedc239900a6e42cc716a71219cd493
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-01-27 19:31:35 +0500

    footer boshlandi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 
 import { Route, Routes } from 'react-router-dom';
-// import Home from './components/home/Home';
+import Home from './components/home/Home';
 import About from './components/about/About';
 import Navbar from './components/navbar';
 import Tickets from './components/ticekts/Tickets';
 
 function App() {
   return (
-    
-      <Routes>
-        {/* <Route path='/' element={<Home/>}/> */}
-        <Route path='/about' element={<About/>}/>
-        <Route path='/Navbar' element={<Navbar/>}/>
-        <Route path='/tickets' element={<Tickets/>}/>
-      </Routes>
-
-      
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/Navbar' element={<Navbar />} />
+      <Route path='/tickets' element={<Tickets />} />
+    </Routes>
   );
 }
 

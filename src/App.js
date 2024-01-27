de9f334830089a94d27a9ddc086f8efa0f952aed
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-27 20:49:32 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { Route, Routes } from 'react-router-dom';
-// import Home from './components/home/Home';
+import Home from './components/home/Home';
 import About from './components/about/About';
 import Navbar from './components/navbar';
 import Tickets from './components/home/tickets/Tickets';
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="container">
       <Routes>
-        {/* <Route path='/' element={<Home/>}/> */}
+        <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/Navbar' element={<Navbar/>}/>
         <Route path='/tickets' element={<Tickets/>}/>

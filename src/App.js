f46cde49ed6d97c91c2c5331a6c1a41bdf196f3b
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-01-27 13:29:29 +0500

    jgjg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Route path='/' />
+      </Router>
+
       </div>
   );
 }

dd19614cea643137e32e77c24bc45e74f987f37b
omadbekolimov <omadbekolimov96@gmail.com>
2024-01-29 17:54:39 +0500

    ketdi

--- src/components/ticekts/components/AboutFirst.jsx
+++ src/components/ticekts/components/AboutFirst.jsx
@@ -2,9 +2,9 @@ import { chipta } from "../../../assets"
 
 function AboutFirst(){
     return(
-    <div className={`flex justify-center items-center w-full rounded-3xl`}>
-    <div className='about-one-page w-4/3 h-[75vh] rounded-3xl'>
-      <img src={chipta} alt="img" className='w-full h-full object-cover relative' />
+    <div className={`flex justify-center items-center w-full  rounded-3xl`}>
+    <div className='about-one-page1 w-1200   h-[75vh] '>
+      <img src={chipta} alt="img" className='w-1200 lg:w-full md:w-300 h-full object-cover relative rounded-3xl' />
     
        
     </div>
--- src/components/ticekts/components/Card.jsx
+++ src/components/ticekts/components/Card.jsx
@@ -3,34 +3,34 @@ import { frame } from "../../../assets"
 
 function Card(){
     return(
-        <div className="flex justify-center gap-4">
+        <div className="flex justify-center gap-4 md: w-full sm:w-full">
         <div className="flex  justify-center px-4 gap-20">
           <div class=" bg-white rounded-xl flex p-10">
             <div class="flex flex-col items-center">
                 <img src={frame} alt="" srcset="" />
-                <h1 className="text-green-600 text-xl ">4 SAR</h1>
+                <h1 className="text-green-600 text-xl font-bold mt-3 ">4 SAR</h1>
                 <p class="mt-2 text-gray-500">2 Hours Pass</p>
             </div>
           </div>
           <div class="p-10 bg-white rounded-xl flex">
             <div class="flex flex-col items-center">
                 <img src={frame} alt="" srcset="" />
-                <h1 className="text-green-600 text-xl ">4 SAR</h1>
-                <p class="mt-2 text-gray-500">2 Hours Pass</p>
+                <h1 className="text-green-600 text-xl font-bold mt-3 ">20 SAR</h1>
+                <p class="mt-2 text-gray-500">3-Day Pass</p>
             </div>
           </div>
           <div class="p-10 bg-white rounded-xl flex">
             <div class="flex flex-col items-center">
                 <img src={frame} alt="" srcset="" />
-                <h1 className="text-green-600 text-xl ">4 SAR</h1>
-                <p class="mt-2 text-gray-500">2 Hours Pass</p>
+                <h1 className="text-green-600 text-xl font-bold  mt-3">40 SAR</h1>
+                <p class="mt-2 text-gray-500">7-Day Pass</p>
             </div>
           </div>
           <div class="p-10 bg-white rounded-xl flex">
             <div class="flex flex-col items-center">
                 <img src={frame} alt="" srcset="" />
-                <h1 className="text-green-600 text-xl ">4 SAR</h1>
-                <p class="mt-2 text-gray-500">2 Hours Pass</p>
+                <h1 className="text-green-600 text-xl font-bold mt-3">140 SAR</h1>
+                <p class="mt-2 text-gray-500">30-Day Pass</p>
             </div>
           </div>
         </div>

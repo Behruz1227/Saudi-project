198af6e1afe1d7717fb8de1584e80cc95c141111
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-01-29 19:38:33 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,14 +14,14 @@ function Home() {
 
   return (
     <>
-      <div className='w-full md:px-20 sm:px-5 px-3 lg:py-10 py-5'>
+      <div className='w-full md:px-20 sm:px-5 px-0 lg:py-10 py-5'>
         {/* <TripplanFirs/> */}
-        {/* <MobilApp/> */}
+        <MobilApp/>
         <Tripplan/>
       </div>
-      <BusPlan/>
-      <MaterSafety />
-      <Galery />
+      {/* <BusPlan/> */}
+      {/* <MaterSafety /> */}
+      {/* <Galery /> */}
     </>
 
   )

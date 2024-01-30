56951d7789f4d65a6ffe5579dbfd7057d0b8085a
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-01-30 15:35:41 +0500

    navbar xam responsiveligi bn buldi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
 import About from './components/about/About';
-import Navbar from './components/navbar';
 import Tickets from './components/ticekts/Tickets';
 import Routerr from './components/Route/routerr';
 import Contactus from './components/contact/contactUs';
 import QuestionsOne from './components/question/QuestionsOne';
+import Navbars from './components/navbar/Navbars';
+// import Navbar from './components/navbar';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/navbar' element={<Navbar />} />
-      <Route path='/routerr' element={<Routerr />} />
-      <Route path='/contactUs' element={<Contactus/>}/>
-      <Route path='/tickets' element={<Tickets />}/>
-      <Route path='/tickets' element={<Tickets />} />
-      <Route path='/question' element={<QuestionsOne/>}/>
-    </Routes>
+    <>
+      <Navbars />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        {/* <Route path='/navbar' element={<Navbar />} /> */}
+        <Route path='/routerr' element={<Routerr />} />
+        <Route path='/contactUs' element={<Contactus />} />
+        {/* <Route path='/tickets' element={<Tickets />} /> */}
+        <Route path='/tickets' element={<Tickets />} />
+        <Route path='/question' element={<QuestionsOne />} />
+      </Routes>
+    </>
   );
 }
 

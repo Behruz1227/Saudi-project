bf75d822a2ae536340b4602ea6bbf6a8969f5bd5
Sardorbek <sardorbekcoderjon@gmail.com>
2024-01-30 18:31:23 +0500

    awdawdawdawd

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Navbars from './components/navbar/Navbars';
 function App() {
   return (
     <>
-      {/* <Navbars /> */}
+      <Navbars />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />

96467123662212780b05f0b4f496f7070f911926
Sardorbek <sardorbekcoderjon@gmail.com>
2024-01-30 17:10:11 +0500

    contact us

--- src/components/Route/routerRow.jsx
+++ src/components/Route/routerRow.jsx
@@ -7,7 +7,7 @@ const RouterRow = ({id}) => {
     <div className='rounded-xl flex md:w-3/4 w-full justify-сenter  items-center'>
       <div class="collapsible-accordion w-full">
         <div class="collapsible-item">
-          <input type="checkbox" id={id} />
+          <input className='router-input' type="checkbox" id={id} />
           <label class="collapsible-item-label" for={id}>
             <img src={busAcardion} alt="ddd"  />
             <button type='button' className=' rounded-3xl sm:py-2 py-0 px-3 bg-[#6FD44F] text-white font-normal'>Bus 160</button>
--- src/components/contact/contactForm.jsx
+++ src/components/contact/contactForm.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { answer, call, message } from '../../assets'
 
 const ContactForm = () => {
   return (
-    <div>
-      <input type="text" placeholder='search' className='bg-black w-10 h-10' />
+    <div className='flex'>
+      <div className='my-3 contactForm'>
+        <div className='mb-3 contactForm1'>
+          <input type="text" placeholder='Email*' required className='InputForm' />
+        </div>
+        <div className='contactForm2' >
+          <textarea className='contactTextarea' placeholder='Type your message here' rows={14} cols={78} />
+        </div>
+      </div>
+      <div className='contactAfisha'>
+        <div className='w-full flex bg-[#FFFFFF] rounded-[0.8rem] ml-5 my-3'>
+          <div className='p-8'>
+            <img src={call} alt="call image"  />
+          </div>
+          <div className='pt-9 items-center'>
+            <p className='text-sm font-medium'>Call Us:</p>
+            <p className='mt-2 font-semibold text-[#6FD44F] text-xs'>Customer Care 19933</p>
+          </div>
+        </div>
+        <div className='w-full flex bg-[#FFFFFF] rounded-[0.8rem] ml-5 my-4'>
+          <div className='p-8'>
+            <img src={message} alt="call image"  />
+          </div>
+          <div className='pt-9 items-center'>
+            <p className='text-sm font-medium'>Email Us:</p>
+            <p className='mt-2 font-semibold text-[#6FD44F] text-xs'>customercare@riyadhbus.sa</p>
+          </div>
+        </div>
+        <div className='w-full flex bg-[#FFFFFF] rounded-[0.8rem] ml-5 my-3'>
+          <div className='p-8'>
+            <img src={answer} alt="call image"  />
+          </div>
+          <div className='pt-9 items-center'>
+            <p className='text-sm font-medium'>FAQs:</p>
+            <p className='mt-2 font-semibold text-[#6FD44F] text-xs'>Have a question?</p>
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
 

29a8bc6cd331fa17bf8346b0c081f7668042b583
Abdulazizsaidovcode <Abdulazizbey5@gmail.com>
2024-02-03 13:50:38 +0500

    salom
    Signed-off-by: Abdulazizsaidovcode <Abdulazizbey5@gmail.com>

--- src/components/ticekts/components/Card.jsx
+++ src/components/ticekts/components/Card.jsx
@@ -1,47 +1,45 @@
-import { frame } from "../../../assets"
+import { useEffect, useState } from "react";
+import { frame } from "../../../assets";
+import axios from "axios";
+import { url } from "../../api";
 
+function Card() {
+  const [tickets, setTickets] = useState([]);
 
-function Card(){
-    return(
-        <div className=" flex justify-center w-full">
-        <div className="flex justify-center px-4 gap-5 cradTicekt">
-          <div className="flex justify-center gap-4 w-full flex-col xl:flex-row">
+  useEffect(() => {
+    getTickets();
+  }, []);
 
-          <div class=" bg-white rounded-xl flex w-[300px] p-10">
-            <div class="flex flex-col items-center w-full">
-                <img src={frame} alt="" srcset="" />
-                <h1 className="text-green-600 text-xl font-bold mt-3 ">4 SAR</h1>
-                <p class="mt-2 text-gray-500">2 Hours Pass</p>
-            </div>
-          </div>
-          <div class="p-10 bg-white rounded-xl w-[300px] flex">
-            <div class="flex flex-col items-center w-full">
-                <img src={frame} alt="" srcset="" />
-                <h1 className="text-green-600 text-xl font-bold mt-3 ">20 SAR</h1>
-                <p class="mt-2 text-gray-500">3-Day Pass</p>
-            </div>
-          </div>
-          </div>
-          <div className="flex justify-center gap-4 w-full flex-col xl:flex-row">
-          <div class="p-10 bg-white rounded-xl w-[300px] flex">
-            <div class="flex flex-col items-center w-full">
-                <img src={frame} alt="" srcset="" />
-                <h1 className="text-green-600 text-xl font-bold  mt-3">40 SAR</h1>
-                <p class="mt-2 text-gray-500">7-Day Pass</p>
-            </div>
-          </div>
-          <div class="p-10 bg-white rounded-xl w-[300px] flex">
-            <div class="flex flex-col items-center w-full">
-                <img src={frame} alt="" srcset="" />
-                <h1 className="text-green-600 text-xl font-bold mt-3">140 SAR</h1>
-                <p class="mt-2 text-gray-500">30-Day Pass</p>
-            </div>
-          </div>
+  function getTickets() {
+    axios
+      .get(`${url}ticket/list`)
+      .then((res) => {
+        setTickets(res.data.body); // `getTicket` dan `setTickets` ga o'zgartirildi, nomlanish standartlariga muvofiqroq
+      })
+      .catch(() => {
+        console.error("Xato: Ma'lumot olishda muammo yuz berdi");
+      });
+  }
+
+  return (
+    <div className="flex md:justify-around justify-center w-full flex-wrap lg:px-56">
+      {/* Tashqi divlarni takrorlash o'rniga, tickets map qilinishi va har bir ticket uchun alohida card yaratilishi mumkin */}
+      {tickets.map((ticket, index) => (
+        <div key={index} className="flex justify-center  px-4 gap-5 cardTicket mt-5">
+          <div className="bg-white rounded-xl flex w-[300px] p-10 flex-col items-center">
+            <img src={frame} alt="Frame" />
+            {/* Mavjud bo'lsa, ticket ma'lumotlarini ko'rsatish */}
+            {ticket && (
+              <>
+                <h1 className="text-green-600 text-xl font-bold mt-3">{ticket.price} {ticket.currency}</h1>
+                <p className="mt-2 text-gray-500">{ticket.pass}</p>
+              </>
+            )}
           </div>
         </div>
-        
-      </div>
-      
-    )
+      ))}
+    </div>
+  );
 }
-export default Card
+
+export default Card;

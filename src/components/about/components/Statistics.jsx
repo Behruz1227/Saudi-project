0a58b5d934bb2266f8494b6b16d86d09a2b63294
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-05 21:30:19 +0500

    sdfgdgb

--- src/components/about/components/Statistics.jsx
+++ src/components/about/components/Statistics.jsx
@@ -1,17 +1,4 @@
-import { useEffect, useState } from "react";
-
 const Statistics = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 15);
-
-    setTimeout(() => { clearInterval(interval); }, 12000);
-
-    return () => clearInterval(interval);
-  }, []);
   return (
     <>
       <div className="flex justify-between items-start max-w-[1100px] mx-auto about-statistics-main">
@@ -20,7 +7,7 @@ const Statistics = () => {
           <p className="text-[.9rem] tracking-wide font-medium leading-4 opacity-90 ml-1">of integrated bus <br /> network</p>
         </div>
         <div className="w-1/3 text-center">
-          <h1 className="text-[2.1rem] font-bold text-green-400 tracking-wider">{counter}+</h1>
+          <h1 className="text-[2.1rem] font-bold text-green-400 tracking-wider">800+</h1>
           <p className="text-[.9rem] tracking-wide font-medium leading-4 opacity-90 mr-11">Buses</p>
         </div>
         <div className="w-1/3 text-left sm:text-end">

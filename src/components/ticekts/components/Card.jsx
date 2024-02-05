69527bce189892de0a7d3e5aeb744bd0e6af4e39
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-05 16:13:17 +0500

    xozirchaga full buldi

--- src/components/ticekts/components/Card.jsx
+++ src/components/ticekts/components/Card.jsx
@@ -11,32 +11,21 @@ function Card() {
   }, []);
 
   function getTickets() {
-    axios
-      .get(`${url}ticket/list`)
-      .then((res) => {
-        setTickets(res.data.body); // `getTicket` dan `setTickets` ga o'zgartirildi, nomlanish standartlariga muvofiqroq
-      })
-      .catch(() => {
-        console.error("Xato: Ma'lumot olishda muammo yuz berdi");
-      });
+    axios.get(`${url}ticket/list`)
+      .then((res) => setTickets(res.data.body))
+      .catch(() => console.error("Xato: Ma'lumot olishda muammo yuz berdi"));
   }
 
   return (
-    <div className="flex md:justify-around justify-center w-full flex-wrap lg:px-56">
-      {/* Tashqi divlarni takrorlash o'rniga, tickets map qilinishi va har bir ticket uchun alohida card yaratilishi mumkin */}
-      {tickets.slice(0,4).map((ticket, index) => (
-        <div key={index} className="flex justify-center px-4 gap-5 cardTicket mt-5">
-          <div className="bg-white hover:bg-green-500 duration-300 rounded-xl flex w-[300px] p-10 flex-col items-center">
-            <img src={frame} alt="Frame" />
-            {/* Mavjud bo'lsa, ticket ma'lumotlarini ko'rsatish */}
-            {ticket && (
-              <>
-                <h1 className="text-green-600 text-xl font-bold mt-3">{ticket.price} {ticket.currency}</h1>
-                <p className="mt-2 text-gray-500">{ticket.pass}</p>
-              </>
-            )}
-          </div>
+    <div className={`max-w-[1230px] flex justify-evenly flex-wrap`}>
+      {tickets && tickets.map((item) => (
+      <div className={`w-full sm:w-[320px] md:w-[256px] lg:w-[256px] xl:w-[307.5px] px-3 py-4`} key={item.id}>
+        <div className={`bg-slate-50 px-8 py-10 shadow-md rounded-3xl hover:shadow-lg text-green-700 hover:text-white hover:bg-[#76e853] border border-slate-300 duration-300`}>
+          <img src={frame} alt="Frame" className="hover:scale-110 duration-200" />
+          <h1 className={`text-[1.5rem] mt-2.5 font-bold tracking-wide uppercase`}>{item.price} {item.currency}</h1>
+          <p className={`text-[.9rem] mt-2 font-sans font-medium text-slate-900 tracking-wider ms-.5`}>{item.pass}</p>
         </div>
+      </div>
       ))}
     </div>
   );

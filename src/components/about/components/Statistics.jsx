477508245c282bc348151973c36781577498ff31
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-05 22:05:41 +0500

    gsap kutubxona urnatildi npm i qilib olinglar

--- src/components/about/components/Statistics.jsx
+++ src/components/about/components/Statistics.jsx
@@ -1,18 +1,62 @@
+import gsap from "gsap";
+import { useRef, useEffect } from "react";
+
 const Statistics = () => {
+  const numberRef = useRef(null);
+  const numberRef2 = useRef(null);
+  const numberRef3 = useRef(null);
+
+  useEffect(() => {
+    startAnimation();
+    startAnimation2();
+    startAnimation3();
+  }, [])
+
+  const startAnimation = () => {
+    gsap.to(numberRef.current, {
+      duration: 10,
+      innerHTML: 1900,
+      roundProps: 'innerHTML',
+      ease: 'power1.out',
+    });
+  };
+  const startAnimation2 = () => {
+    gsap.to(numberRef2.current, {
+      duration: 10,
+      innerHTML: 800,
+      roundProps: 'innerHTML',
+      ease: 'power1.out',
+    });
+  };
+  const startAnimation3 = () => {
+    gsap.to(numberRef3.current, {
+      duration: 10,
+      innerHTML: 2900,
+      roundProps: 'innerHTML',
+      ease: 'power1.out',
+    });
+  };
+
   return (
     <>
       <div className="flex justify-between items-start max-w-[1100px] mx-auto about-statistics-main">
         <div className="w-1/3">
-          <h1 className="text-[2.1rem] font-bold text-green-400 tracking-wider"><span>1900</span>+ km</h1>
+          <h1 className="text-[2.1rem] font-bold text-green-400 tracking-wider">
+            <span ref={numberRef}>0</span>+ km</h1>
           <p className="text-[.9rem] tracking-wide font-medium leading-4 opacity-90 ml-1">of integrated bus <br /> network</p>
         </div>
         <div className="w-1/3 text-center">
-          <h1 className="text-[2.1rem] font-bold text-green-400 tracking-wider">800+</h1>
+          <h1 className="text-[2.1rem] font-bold text-green-400 tracking-wider">
+            <span ref={numberRef2}>0</span>
+            <span>+</span>
+          </h1>
           <p className="text-[.9rem] tracking-wide font-medium leading-4 opacity-90 mr-11">Buses</p>
         </div>
         <div className="w-1/3 text-left sm:text-end">
-          <h1 className="text-[2.1rem] font-bold text-green-400 tracking-wider">2,900+</h1>
-          <p className="text-[.9rem] tracking-wide font-medium leading-4 opacity-90 mr-9">Bus Stations</p>
+          <h1 className="text-[2.1rem] font-bold text-green-400 tracking-wider">
+            <span ref={numberRef3}>0</span>+
+          </h1>
+          <p className="text-[.9rem] tracking-wide font-medium leading-4 opacity-90 mr-6">Bus Stations</p>
         </div>
       </div>
     </>

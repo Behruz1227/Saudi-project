709c165cbf070e8fd4eaa8ab72ab7050eb5b5bc2
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-02-06 15:06:41 +0500

    gdttf

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -5,13 +5,13 @@ import "./location.css";
 
 const Location = () => {
     const [fixedBar, getFixedbar] = useState(false)
-
+    const classcha = "h-0"
     function setFixedBar() {
         getFixedbar(!fixedBar)
     }
     return (
         <div className="location-main w-full flex justify-between md:flex-row flex-col-reverse items-start">
-            <div className={`md:w-1/4 md:block md:relative fixed md:top-0 bottom-0   z-20 transition-all ${fixedBar ? "" : "-bottom-[360px]"} `}>
+            <div className={`md:w-1/4 md:block md:relative fixed md:top-0 bottom-0 z-20 transition-all ease-in-out delay-150 ${fixedBar ? "" : classcha + " transition-all"} `}>
                 <LocationSidebar />
                 <button onClick={setFixedBar} className={`absolute -top-10 md:hidden   btn-bg rounded-t-full p-2 w-20 h-10 flex justify-center items-center bg-slate-500 `}>{fixedBar ? <i class="fa-solid fa-arrow-down"></i> :  <i class="fa-solid fa-arrow-up"></i>}</button>
             </div>
